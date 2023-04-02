4ffb81682737f2b222463bcc0179cde968edd539
techno303games <129646624+techno303games@users.noreply.github.com>
2023-04-02 16:33:48 +1000

    Added code from Python editor.
    I have added the code from the Microsoft MakeCode Python editor.

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-input.onButtonPressed(Button.A, function () {
-    number += 1
-})
-let number = 0
+number = 0
+
+def on_button_pressed_a():
+    global number
+    music.play_melody("C E E F F - - - ", 0)
+    while True:
+        number += 1
+        basic.show_string("" + str((number)))
+        basic.pause(1000)
+input.on_button_pressed(Button.A, on_button_pressed_a)
